8d5325d352798f0394c5e4a10ba79cb654b80e61
Evan Brown <brownev19@students.ecu.edu>
2022-10-24 18:05:21 -0400

    Added more HTML code, added CSS and JS files for a sticky header

---
+++ PiratesSpotlightJS.js
@@ -0,0 +1,19 @@
+// JavaScript Document
+
+// Header Style
+window.onscroll = function() {headerFunction()};
+
+var header = document.getElementById("pageHeader");
+
+var sticky = header.offsetTop;
+
+function headerFunction() {
+	if (window.pageYOffset > sticky) {
+		header.classList.add("sticky");
+	}
+	else {
+		header.classList.remove("sticky");
+	}
+}
+
+// Header Style End
